4b5abe3846713135fb8965d251f0e34d73da1211
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-04 22:17:04 +0300

    feat: add task 4 copy directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+    if (err) console.error(err);
+  });
+  fs.readdir(
+    path.join(__dirname, 'files'),
+    { withFileTypes: true },
+    (error, files) => {
+      if (error) {
+        console.log(error);
+      } else {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            fs.createReadStream(path.join(__dirname, 'files', file.name)).pipe(
+              fs.createWriteStream(
+                path.join(__dirname, 'files-copy', file.name)
+              )
+            );
+          }
+        });
+      }
+    }
+  );
+}
+
+copyDir();
