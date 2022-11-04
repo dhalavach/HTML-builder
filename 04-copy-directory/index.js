8c6fbc3cbe1881409af2a42b9d1b14ee678be51b
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-05 00:16:05 +0300

    feat: async check and remove directory in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,40 +4,43 @@ const path = require('path');
 function copyDir() {
   let destination = path.join(__dirname, 'files-copy');
 
-  // fs.access(destination, (error) => {
-  //   error ? console.log('does not exist')
-  //     : fs.rm(destination, { recursive: true, force: true }, (error) => {
-  //         if (error) console.log(error);
-  //       });
-  // });
-
-  if (fs.existsSync(destination)) {
-    fs.rmSync(destination, { recursive: true, force: true }, (error) => {
-      if (error) console.log(error);
-    });
-    console.log('folder deleted');
-  }
-
-  fs.mkdir(destination, { recursive: true }, (err) => {
-    if (err) console.error(err);
-  });
-  fs.readdir(
-    path.join(__dirname, 'files'),
-    { withFileTypes: true },
-    (error, files) => {
-      if (error) {
-        console.log(error);
-      } else {
-        files.forEach((file) => {
-          if (file.isFile()) {
-            fs.createReadStream(path.join(__dirname, 'files', file.name)).pipe(
-              fs.createWriteStream(path.join(destination, file.name))
-            );
-          }
+  fs.access(destination, (error) => {
+    error
+      ? copy()
+      : fs.rm(destination, { recursive: true, force: true }, () => {
+          copy();
         });
+  });
+
+  // if (fs.existsSync(destination)) {
+  //   fs.rmSync(destination, { recursive: true, force: true }, (error) => {
+  //     if (error) console.log(error);
+  //   });
+  //   console.log('folder deleted');
+  // }
+
+  function copy() {
+    fs.mkdir(destination, { recursive: true }, (err) => {
+      if (err) console.error(err);
+    });
+    fs.readdir(
+      path.join(__dirname, 'files'),
+      { withFileTypes: true },
+      (error, files) => {
+        if (error) {
+          console.log(error);
+        } else {
+          files.forEach((file) => {
+            if (file.isFile()) {
+              fs.createReadStream(
+                path.join(__dirname, 'files', file.name)
+              ).pipe(fs.createWriteStream(path.join(destination, file.name)));
+            }
+          });
+        }
       }
-    }
-  );
+    );
+  }
 }
 
 copyDir();
