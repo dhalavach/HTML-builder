0f012415621d833f2271fd0a4c3e94f1298b59fe
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-04 21:56:55 +0300

    fix: task 3 typo correction

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,33 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(
-  path.join(__dirname, 'secret-folder'),
-  { withFileTypes: true },
-  (error, files) => {
-    if (error) {
-      console.log(error);
-    } else {
-      files.forEach((file) => {
-        if (file.isFile()) {
-          fs.stat(
-            path.join(__dirname, 'secret-folder', file.name),
-            (error, fileStats) => {
-              if (error) console.log(error);
-              console.log(
-                path.parse(file.name).name +
-                  ' - ' +
-                  path.extname(
-                    path.join(__dirname, 'secret-folder', file.name)
-                  ) +
-                  ' - ' +
-                  fileStats.size / 1000 +
-                  ' Kb'
-              );
-            }
-          );
-        }
-      });
+function listNames() {
+  fs.readdir(
+    path.join(__dirname, 'secret-folder'),
+    { withFileTypes: true },
+    (error, files) => {
+      if (error) {
+        console.log(error);
+      } else {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            fs.stat(
+              path.join(__dirname, 'secret-folder', file.name),
+              (error, fileStats) => {
+                if (error) console.log(error);
+                console.log(
+                  path.parse(file.name).name +
+                    ' - ' +
+                    path.extname(
+                      path.join(__dirname, 'secret-folder', file.name)
+                    ) +
+                    ' - ' +
+                    fileStats.size / 1000 +
+                    ' Kb'
+                );
+              }
+            );
+          }
+        });
+      }
     }
-  }
-);
+  );
+}
+
+listNames();
--- 03-files-in-folder/test.js
+++ 03-files-in-folder/test.js
@@ -1,2 +1,1 @@
-function getFileInfo(file) {
-}
+
