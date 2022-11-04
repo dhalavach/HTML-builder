47b43b016aa81f16e5ad03e98c18ca68f94a1517
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-04 17:17:00 +0300

    chore: remove comments

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,29 +5,6 @@ const path = require("path");
 const readline = require("node:readline");
 const { stdout } = require("process");
 
-// //1
-// const { stdin, stdout } = process;
-// let exitCommand = "exit";
-// const output = fs.createWriteStream(path.join(__dirname, "output.txt"));
-// stdout.write("Hello! Please enter the text and press ENTER: ");
-
-// stdin.on("data", (data) => {
-//   if (data.toString().toLowerCase() === exitCommand) {
-//     stdout.write("Exiting... Bye! ");
-//     output.close();
-//     process.exit();
-//   } else {
-//     output.write(data);
-//   }
-// });
-
-// process.on("SIGINT", () => {
-//   stdout.write("Ctrl-C exiting ... Bye!");
-//   output.close();
-//   process.exit();
-// });
-
-//2
 const writeStream = fs.createWriteStream(path.join(__dirname, "output.txt"));
 const rl = readline.createInterface(process.stdin, process.stdout);
 const exitCommand = "exit";
