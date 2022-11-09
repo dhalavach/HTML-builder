c1a4d3e141983dd724554a170d98438bb3b9ab3f
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-10 01:08:41 +0300

    chore: format code and remove comments

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,5 @@
-/* eslint-disable */
 const fs = require('fs');
 const path = require('path');
-
-// fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (error, data) => {
-//   if (error) throw error;
-//   console.log(data);
-// });
 
 const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 const { stdout } = process;
