9912ef6f49e117319b9e73463221a52672e492f5
Dzmitry Halavach <kotik1@protonmail.com>
2022-11-09 20:06:14 +0300

    fix: task 2 writing to file line by line

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,23 +3,24 @@ const path = require('path');
 const readline = require('node:readline');
 const { stdout } = require('process');
 
-const writeStream = fs.createWriteStream(path.join(__dirname, 'output.txt'));
-const rl = readline.createInterface(process.stdin, process.stdout);
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const exitCommand = 'exit';
+const output = 'output.txt'
 
 stdout.write("Enter text and press ENTER or type 'exit' to leave: ");
 
 rl.on('line', (line) => {
-  if (line.toString().toLowerCase() === exitCommand) {
-    writeStream.close();
+  if (line.toString().toLowerCase().trim() === exitCommand) {
     rl.close();
     process.exit(0);
   } else {
-    writeStream.write(line + '\n');
+    fs.writeFile(path.resolve(__dirname, output), line + '\n', { encoding: 'utf8', flag: 'a' }, (err) => {
+      if (err) throw err;
+    })
   }
 });
 
 rl.on('close', function () {
-  console.log('\nBYE!');
+  stdout.write('\nBYE!');
   process.exit(0);
 });
